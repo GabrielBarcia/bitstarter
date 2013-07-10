5aed2150f76303dde592d3f125861f7e93f996ed
GabrielBarcia <gabriel.barcia@gmail.com>
2013-07-10 04:09:37 +0000

    Added right version of the web.js for reading index.html v4

--- web.js
+++ web.js
@@ -2,11 +2,9 @@ var express = require('express');
 var fs = require ('fs');
 var app = express.createServer(express.logger());
 
-App.get('/', function(request, response) {
-  response.send( 'test');
-});
-
-// fs.readFileSync('index.html').toString() );
+app.get('/', function(request, response) {
+  response.send(  fs.readFileSync('index.html').toString() );
+};
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
